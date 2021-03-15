8d60928588a8825f7d5e2eb8267df0aad5fb2171
Tom Trogdon <trogdon@uw.edu>
2021-03-15 16:49:18 -0700

    Fixed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
     n = 20
     A = randn(n,n)/(2*sqrt(n)) + I
     b = randn(n,1)
-    out = GMRES(x -> A*x, b, (x,y) -> (y'*x)[1], 1e-10, n+1)
+    out = GMRES(x -> A*x, b, (x,y) -> (y'*x)[1], 1e-10, n+1, x -> x)
     sol = sum([out[2][i]*out[1][i] for i=1:length(out[2]) ])
     @test norm(A*sol-b) < 1e-10
 end
